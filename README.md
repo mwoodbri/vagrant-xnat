025e9d1a35227363c814a40e2d87f838ed1119b5
Mark Woodbridge <mark.woodbridge+git@gmail.com>
2014-03-18 09:28:09 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-vagrant-xnat
-============
+XNAT installer (Vagrant)
+========================
+
+To automatically setup XNAT:
+
+1. Install [Vagrant](http://www.vagrantup.com/)
+1. ```git clone https://github.com/mwoodbri/vagrant-xnat.git```
+1. ```cd vagrant-xnat```
+1. ```vagrant up```
+1. Visit <http://localhost:8080/xnat>
---
+++ install-xnat.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -eux
+
+DEPS=$1
+OWNER=$2
+GROUP=$3
+XNAT_DATA=$4
+
+RULENUM=$(iptables -L INPUT --line-numbers | grep 'REJECT' | awk '{print $1}')
+iptables -I INPUT $RULENUM -m state --state NEW -m tcp -p tcp --dport 8080 -j ACCEPT
+service iptables save
+
+yum install -y postgresql-server
+service postgresql initdb
+sed -i 's/ident$/trust/' /var/lib/pgsql/data/pg_hba.conf
+service postgresql start
+createuser -U postgres -S -D -R xnat01
+createdb -U postgres -O xnat01 xnat
+createlang -U postgres -d xnat plpgsql
+
+if [ ! -f $DEPS/server-jre-7u51-linux-x64.tar.gz ]; then
+	echo 'Please download server-jre-7u51-linux-x64.tar.gz and place it this directory'
+	exit 1
+fi
+tar xf $DEPS/server-jre-7u51-linux-x64.tar.gz -C /opt
+export JAVA_HOME=/opt/jdk1.7.0_51
+
+if [ ! -f $DEPS/apache-tomcat-7.0.52.tar.gz ]; then
+	cd $DEPS
+	curl -O http://www.mirrorservice.org/sites/ftp.apache.org/tomcat/tomcat-7/v7.0.52/bin/apache-tomcat-7.0.52.tar.gz
+fi
+tar xf $DEPS/apache-tomcat-7.0.52.tar.gz -C /opt
+TOMCAT_HOME=/opt/apache-tomcat-7.0.52
+
+if [ ! -f $DEPS/xnat-1.6.3.tar.gz ]; then 
+	cd $DEPS
+	curl -O ftp://ftp.nrg.wustl.edu/pub/xnat/xnat-1.6.3.tar.gz
+fi
+tar xf $DEPS/xnat-1.6.3.tar.gz -C /opt
+XNAT_HOME=/opt/xnat
+
+mkdir $XNAT_DATA
+
+ln -s $DEPS/build.properties $XNAT_HOME
+
+cd $XNAT_HOME
+export PATH=$JAVA_HOME/bin:$PATH
+bin/setup.sh -Ddeploy=true
+
+cd $XNAT_HOME/deployments/xnat
+psql -d xnat -f sql/xnat.sql -U xnat01
+$XNAT_HOME/bin/StoreXML -l security/security.xml -allowDataDeletion true
+$XNAT_HOME/bin/StoreXML -dir ./work/field_groups -u admin -p admin -allowDataDeletion true
+
+chown -R $OWNER.$GROUP $XNAT_HOME $TOMCAT_HOME $XNAT_DATA
+
+su $OWNER $TOMCAT_HOME/bin/startup.sh
